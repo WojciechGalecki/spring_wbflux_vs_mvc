4dae5fe6ba48ea634b1ebd9bbea2d6be39cc9b8c
WojciechGalecki <wojciech.galecki@wp.pl>
2022-02-12 13:09:02 +0100

    init commit

---
+++ blocking-service/src/main/java/wg/blocking/BlockingServiceApplication.java
@@ -0,0 +1,11 @@
+package wg.blocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlockingServiceApplication.class, args);
+    }
+}
---
+++ reactive-service/src/main/java/wg/reactive/ReactiveServiceApplication.java
@@ -0,0 +1,11 @@
+package wg.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveServiceApplication.class, args);
+    }
+}
